9374e2620de5504018ed1d0e388c2e46cd781fd3
trevoramann <trevor.amann9@gmail.com>
2021-03-15 18:05:57 -0500

    Finished

--- Assignments/Program2/Amann_Prog2Orig.cpp
+++ Assignments/Program2/Amann_Prog2Orig.cpp
@@ -1,3 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//                   
+// Author:           Trevor Amann
+// Email:            trevor.amann9@gmail.com
+// Label:            Program 2
+// Title:            Functions
+// Course:           CMPS 2433
+// Semester:         Spring 2021
+//
+// Description:
+//    This program computes functions and the compositions of those functions. 
+//
+// Usage:
+//       Enter the name of input file and name your output file and then it will run no problem.
+//
+// Files:   Amann_Prog2Orig.cpp and P2Fn.dat
+/////////////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 #include <cmath>
 #include <fstream>
@@ -5,6 +23,7 @@
 
 using namespace std;
 
+//This function opens the input file.
 void openFiles(ifstream&infile,ofstream&outfile) {
     char inFileName[40];
     char outFileName[40];
@@ -16,48 +35,48 @@ void openFiles(ifstream&infile,ofstream&outfile) {
     outfile.open(outFileName);//open out putfile}
 }
 
-double gans;
-double finvans; 
-double ginvans; 
-double ansfofg;
-double ansgoff;
-double finvoffans;
-double ginvofgans;
-
-
+//This function calculates f(x).
 double fofx(double x){
   double fans = 3*x + 5;
   return fans;
 }
+//This function calculates g(x).
 double gofx(double x){
   double gans = 3*x/5;
   return gans;
 }
+//This function calculates f-1(y).
  double finv(double y){
   double finvans = (y - 5)/3;
   return finvans;
  }
+ //This function calculates g-1(y).
  double ginv(double y){
    double ginvans = (5 * y - 5)/3;
    return ginvans;
  }
+ //This function calculates f(g(x)).
  double fofg(double x){
    double ansfofg = ((9*x)/5) + 8;
    return ansfofg;
  }
+ //This function calculates g(f(x)).
  double goff(double x){
    double ansgoff = ((9*x)/5) + 4;
    return ansgoff;
  }
+ //This function calculates f-1(f(x)).
  double finvf(double x){
    return x;
  } 
+ //This function calculates g-1(g(x)).
  double ginvg(double x){
    return x; 
  }
 
 
-
+//This is the main of the function. This is where we call our functions and find the values we are looking for. Also we 
+//print the values to an output file that we name.
 int main() {
   ifstream infile;
   ofstream outfile;
@@ -65,7 +84,7 @@ int main() {
   int i; 
 
   openFiles(infile, outfile);
-  outfile << "Trevor Amann" << endl << "Program 2: Function Composition" << endl;
+  outfile << "Trevor Amann" << endl << "Program 2: Function Composition Modified" << endl;
   outfile << endl;
   outfile << "f(x) = 3x + 5" << "     " << "g(x) = 3x/5" << "     " << "f-1(y) = (y - 5)/3" << "     " << "g-1(y) = (5y - 5)/3" << endl;
   outfile << endl;
@@ -75,27 +94,28 @@ int main() {
   outfile << ' ' << setw(8) << "fInv(y)";
   outfile << ' ' << setw(8) << "gInv(y)";
   outfile << ' ' << setw(8) << "f(g(x))";
-  outfile << ' ' << setw(8) << "g(f(x))";
+  outfile << ' ' << setw(7) << "g(f(x))";
   outfile << ' ' << setw(8) << "fInv(f(x))"; 
-  outfile << ' ' << setw(8) << "gInv(g(x)) " << endl; 
+  outfile << ' ' << setw(7) << "gInv(g(x)) " << endl; 
   for(i = 0; i < 84; i++){
       outfile << "-";
   } 
   outfile << endl;
 
+//This is where the function calls begin. 
   cout << setprecision(4);
   while (!infile.eof()) {
     infile >> value;
     cout << "New value: " << value << endl;
-    outfile << setw(4) << value;
-    outfile << ' ' << setw(8) << fofx(value);
-    outfile << ' ' << setw(8) << gofx(value);
-    outfile << ' ' << setprecision(2) << setw(8) << finv(value);
-    outfile << ' ' << setw(8) << ginv(value);
-    outfile << ' ' << setw(8) << fofg(value);
-    outfile << ' ' << setprecision(5) << setw(8) << goff(value);
-    outfile << ' ' << setw(10) << finvf(value);
-    outfile << ' ' << setw(10) << ginvg(value);
+    outfile << setw(4) << fixed << setprecision(2) <<  value;
+    outfile << ' ' << setw(6) << fixed << setprecision(2) << fofx(value);
+    outfile << ' ' << setw(6) << fixed << setprecision(2) << gofx(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << finv(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << ginv(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << fofg(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << goff(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << finvf(value);
+    outfile << ' ' << setw(8) << fixed << setprecision(2) << ginvg(value);
     outfile << endl;
   }
 }
